606b370b04c24b479caa48fbdd8289736343cc0e
Mohamed Zahran <mohamed.zahran1@live.com>
2022-08-09 20:00:54 +0200

    refactoring

--- src/Reducers/index.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import cartReducer from "./cart.reducer";
-import orderReducer from "./order.reducer"
-import newOrderReducer from './newOrder.reducer'
-
-const reducers = combineReducers({ cartReducer, orderReducer, newOrderReducer});
-
-export default reducers
